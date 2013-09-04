f3328cb250db41ac7b862225bce7363506cc2403
amber-h <amber.houle3@gmail.com>
2013-09-04 11:56:28 -0400

    defining create method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,12 +4,22 @@ class PostsController < ApplicationController
 	end
 
 	def show 
+		@post = Post.find(params[:id])
 	end
 
 	def new 
+		@post = Post.new
 	end
 
 	def create
+		@post = Post.create params[:post]
+
+		if @post.valid?
+			@post.save
+			redirect_to @post
+		else
+			render @post
+		end
 	end
 
 	def edit
